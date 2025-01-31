27594e95ddd5eac98d83bbf34680dc6dd768c4c2
Tim Waterhouse <timw@metrumrg.com>
2025-01-30 21:40:32 -0500

    add achordion

--- keyboards/keebio/iris_ce/keymaps/timwaterhouse/keymap.c
+++ keyboards/keebio/iris_ce/keymaps/timwaterhouse/keymap.c
@@ -67,3 +67,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   )
 };
+
+#include "features/achordion.h"
+
+bool process_record_user(uint16_t keycode, keyrecord_t* record) {
+  if (!process_achordion(keycode, record)) { return false; }
+  // Your macros ...
+
+  return true;
+}
+
+void matrix_scan_user(void) {
+  achordion_task();
+}
+
